65ee90eec93275dfd9c268fecef13b02e9a334c2
Tim Ker <tim.ker@gmail.com>
2012-09-09 23:25:33 +0100

    added build script

--- Build/buildSearchCenter.ps1
+++ Build/buildSearchCenter.ps1
@@ -1,3 +1,6 @@
-getfrom trunk
-increment version
-zip
+#todo
+#check for existance of zip, maybe even store 7z in extenal folder
+#delete old verson
+#update vesion number
+
+& "C:\Program Files (x86)\7-Zip\7z.exe" a "..\deploy\searchcenter.zip" "..\searchcenterExtension\*.*" -r
