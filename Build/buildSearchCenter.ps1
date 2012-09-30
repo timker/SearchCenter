bbe51b2a6bb9c09f9fea2a931f481ec700d92649
Tim Ker <tttttt>
2012-09-30 20:02:19 +0100

    cleaned up build script

--- Build/buildSearchCenter.ps1
+++ Build/buildSearchCenter.ps1
@@ -1,17 +1,15 @@
 #todo
 #open powershell
 
-#check for existance of zip, maybe even store 7z in extenal folder
-#delete old verson
-# test for existance before deletion
+#check for existence of zip, maybe even store 7z in extenal folder
+#delete old version
+# test for existence before deletion
 #update version number
 #tag
 
 $deploylocation = "..\deploy\searchcenter.zip"
 Remove-Item $deploylocation
 & "C:\Program Files (x86)\7-Zip\7z.exe" a $deploylocation "..\searchcenterExtension\*.*" -r
-
-
 
 Start-Process "https://chrome.google.com/webstore/developer/edit/ndfplmdnbnefomnjiknbpejdceedhdmf"
 
