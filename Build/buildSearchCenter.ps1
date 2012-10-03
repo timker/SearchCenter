28ff746613ec3dee61855a6adbbc008be5d5158f
Tim Ker <tttttt>
2012-10-03 08:06:05 +0100

    updated build script

--- Build/buildSearchCenter.ps1
+++ Build/buildSearchCenter.ps1
@@ -26,9 +26,25 @@ $deploylocation = $deployFolder + $filename
 Remove-Item $deploylocation
 & "C:\Program Files (x86)\7-Zip\7z.exe" a $deploylocation "..\searchcenterExtension\*.*" -r
 
-Start-Process "https://chrome.google.com/webstore/developer/edit/ndfplmdnbnefomnjiknbpejdceedhdmf"
 
+function OpenInstallLocation (){
+    write-Host "opening webstore" -foreground "yellow"
+    Start-Process "https://chrome.google.com/webstore/developer/edit/ndfplmdnbnefomnjiknbpejdceedhdmf"
+}
+
+function CopyText ($text) {
+#http://technet.microsoft.com/en-us/library/ee177031.aspx
+#http://www.itidea.nl/index.php/powershell-choose-between-colored-host-text-or-write-to-an-output-file-not-anymore/
+ write-Host "currently broken" -foreground "red"
+    write-Host "cp text" -foreground "yellow" -nonewline;
+write-Host  $text -foreground "yellow"
+}
 #new-alias  Out-Clipboard $env:SystemRoot\system32\clip.exe
 $fullpath = resolve-path $deploylocation | select Path
-write-output $fullpath
+write-Host copying $fullpath -foreground "yellow"
+#write-host (2 + 2) -foreground "magenta"
 $fullpath | foreach { $_.Path } | . $env:SystemRoot\system32\clip.exe
+
+#OpenInstallLocation
+$fullpath | foreach { $_.Path } | CopyText
+#CopyText "asdf"
